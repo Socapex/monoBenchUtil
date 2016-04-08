6f03f6ef06482dcf4cf997608367f09fa9b51468
pgroarke <pgroarke>
2016-04-08 18:27:17 -0400

    Initial commit

---
+++ Assets/monoBenchUtil.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public static class Bench {
+	static System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+	static System.TimeSpan elapsedTime;
+
+	public static string Title(string message)
+	{
+		string ret = "\n" + new string('#', message.Length) + "\n";
+		ret += message + "\n";
+		ret += new string('#', message.Length) + "\n";
+		Debug.Log(ret);
+		return ret;
+	}
+
+	public static string Start(string message = "")
+	{
+		string ret = "";
+		if (message.Length != 0) {
+			ret = "\n" + message + "\n";
+			ret += new string('-', message.Length) + "\n";
+			Debug.Log(ret);
+		}
+		timer.Reset();
+		timer.Start();
+		return ret;
+	}
+
+	public static string End(string message = "")
+	{
+		timer.Stop();
+		elapsedTime = timer.Elapsed;
+
+		string t = System.String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+			elapsedTime.Hours, elapsedTime.Minutes, elapsedTime.Seconds,
+			elapsedTime.Milliseconds / 10);
+
+		string ret = "\n" + message + "   " + t + "\n";
+		Debug.Log(ret);
+		return ret;
+	}
+}
---
+++ Assets/structs vs classes/struct_v_class.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.CompilerServices;
+
+struct TestStruct {
+	public int patate;
+}
+
+class TestClass {
+	public int patate;
+}
+
+public class struct_v_class : MonoBehaviour {
+	[SerializeField] Text uiText;
+
+	const int qty = 100000000;
+	TestStruct[] structs = new TestStruct[qty];
+	TestClass[] classes = new TestClass[qty];
+
+	void Init()
+	{
+		for (int i = 0; i < qty; ++i) {
+			structs[i] = new TestStruct();
+		}	
+		for (int i = 0; i < qty; ++i) {
+			classes[i] = new TestClass();
+		}
+		uiText.text += Bench.Title("Structs vs class - Linear iteration.");
+	}
+
+	[MethodImpl(MethodImplOptions.NoInlining)]
+	void DoStruct()
+	{
+		uiText.text += Bench.Start();
+		for (int i = 0; i < qty; ++i) {
+			structs[i].patate = 42;
+			//structs[i].patate += 42;
+		}
+		uiText.text += Bench.End("Structs");
+	}
+
+	[MethodImpl(MethodImplOptions.NoInlining)]
+	void DoClass()
+	{
+		uiText.text += Bench.Start();
+		for (int i = 0; i < qty; ++i) {
+			classes[i].patate = 42;
+			//classes[i].patate += 42;
+		}
+		uiText.text += Bench.End("Classes");
+	}
+
+	void Awake()
+	{
+		Init();
+	}
+	void OnEnable()
+	{
+		DoStruct();
+		DoClass();
+	}
+}
